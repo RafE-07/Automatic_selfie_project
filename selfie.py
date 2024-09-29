30f01a44e668ef700b55b3451cd10c32cf452fb8
rafi <mahmudrezarafi@gmail.com>
2024-09-29 18:39:44 +0600

    added selfie project

--- selfie.py
+++ selfie.py
@@ -7,7 +7,7 @@ smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 while True:
     ret, frame = cap.read()
-    oreginal_frame = frame.copy()
+    original_frame = frame.copy()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(gray,1.3,5)
     for x,y,w,h in face:
@@ -18,8 +18,8 @@ while True:
         for x1, y1, w1, h1 in smile:
             cv2.rectangle(face_roi,(x1,y1),(x1+w1,y1+h1),(0,255,255),2)
             time_stamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-            file_name = f'selfee{time_stamp}.png'
-            cv2.imwrite(file_name,oreginal_frame)
+            file_name = f'selfie{time_stamp}.png'
+            cv2.imwrite(file_name,original_frame)
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(10) == ord('q'):
